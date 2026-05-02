ebc6fc2f29c8da6f63eb013deca86c165e00c5ba
ankush badgujar <ankushbadgujar2345@gmail.com>
2026-05-02 12:29:50 +0530

    fixed routing and api url

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -26,7 +26,7 @@ const Register = () => {
         }
 
         try {
-            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/register`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
